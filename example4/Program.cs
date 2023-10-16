3253c8536641eb4955098bf739c70634f8705355
docAlexxx <docalex@mail.ru>
2023-10-16 22:58:57 +0400

    example4 change

--- example4/Program.cs
+++ example4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberOne = 5;
+double numberTwo = 10;
+Console.WriteLine(numberOne/numberTwo);
+numberOne = new Random().Next(1,10);
+numberTwo = new Random().Next(1,10);
+Console.WriteLine(numberOne);
+Console.WriteLine(numberTwo);
+Console.WriteLine(numberOne/numberTwo);
